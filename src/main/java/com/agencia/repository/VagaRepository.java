4a87157b9ae121ec82d207c846e93604e65b1e4f
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2024-06-25 20:36:57 -0300

    realizando o filtro da exclusao logica

--- src/main/java/com/agencia/repository/VagaRepository.java
+++ src/main/java/com/agencia/repository/VagaRepository.java
@@ -3,8 +3,12 @@ package com.agencia.repository;
 import com.agencia.domain.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VagaRepository  extends JpaRepository<Vaga, Long> {
-    Optional<Vaga> findByTitulo(String descricao);
+
+    List<Vaga> findByExclusaoLogicaFalse();
+
+    Optional<Vaga> findByIdAndExclusaoLogicaFalse(Long id);
 }
